5266199374e518ac5f468e691a391fe8aea1eca3
MeowYue <gzgtwz74@gmail.com>
2020-05-12 23:23:28 +0800

    Initial Push

---
+++ Assets/Grass.shader
@@ -0,0 +1,150 @@
+Shader "Roystan/Grass"
+{
+    Properties
+    {
+		[Header(Shading)]
+		_TopColor("Top Color", Color) = (1,1,1,1)
+		_BottomColor("Bottom Color", Color) = (1,1,1,1)
+		_TranslucentGain("Translucent Gain", Range(0,1)) = 0.5
+		_BendRotationRandom("Bend Rotation Random", Range(0, 1)) = 0.2
+		_BladeWidth("Blade Width", Float) = 0.05
+		_BladeWidthRandom("Blade Width Random", Float) = 0.02
+		_BladeHeight("Blade Height", Float) = 0.5
+		_BladeHeightRandom("Blade Height Random", Float) = 0.3
+		_TessellationUniform("Tessellation Uniform", Range(1, 64)) = 1
+	}
+
+		CGINCLUDE
+	#include "UnityCG.cginc"
+	#include "Autolight.cginc"
+	#include "Shaders/CustomTessellation.cginc"
+
+	float _BendRotationRandom;
+	float _BladeHeight;
+	float _BladeHeightRandom;
+	float _BladeWidth;
+	float _BladeWidthRandom;
+
+	//struct vertexInput {
+	//		float4 vertex : POSITION;
+	//		float3 normal : NORMAL;
+	//		float4 tangent : TANGENT;
+	//};
+	//struct vertexOutput {
+	//		float4 vertex : SV_POSITION;
+	//		float3 normal : NORMAL;
+	//		float4 tangent : TANGENT;
+	//};
+	struct geometryOutput {
+		float4 pos : SV_POSITION;
+		float2 uv : TEXCOORD0;
+	};
+
+	geometryOutput VertexOutput(float3 pos, float2 uv) {
+		geometryOutput o;
+		o.pos = UnityObjectToClipPos(pos);
+		o.uv = uv;
+		return o;
+	}
+
+	// Simple noise function, sourced from http://answers.unity.com/answers/624136/view.html
+	// Extended discussion on this function can be found at the following link:
+	// https://forum.unity.com/threads/am-i-over-complicating-this-random-function.454887/#post-2949326
+	// Returns a number in the 0...1 range.
+	float rand(float3 co)
+	{
+		return frac(sin(dot(co.xyz, float3(12.9898, 78.233, 53.539))) * 43758.5453);
+	}
+
+
+	float4x4 AngleAxis4x4(float angle, float3 axis)
+	{
+		float c, s;
+		sincos(angle, s, c);
+
+		float t = 1 - c;
+		float x = axis.x;
+		float y = axis.y;
+		float z = axis.z;
+
+		return float4x4(
+			t * x * x + c, t * x * y - s * z, t * x * z + s * y, 0,
+			t * x * y + s * z, t * y * y + c, t * y * z - s * x, 0,
+			t * x * z - s * y, t * y * z + s * x, t * z * z + c, 0,
+			0,0,0,1
+			);
+	}
+
+	//vertexOutput vert(vertexInput v)
+	//{
+	//	vertexOutput o;
+	//	o.vertex = v.vertex;
+	//	o.normal = v.normal;
+	//	o.tangent = v.tangent;
+	//	return o;
+	//}
+
+	
+
+	[maxvertexcount(3)]
+	void geo(triangle vertexOutput IN[3] : SV_POSITION, inout TriangleStream<geometryOutput> triStream) {
+		float4 pos = IN[0].vertex;
+
+		float3 vNormal = IN[0].normal;
+		float4 vTangent = IN[0].tangent;
+		float3 vBinormal = cross(vNormal, vTangent) * vTangent.w;
+		float4x4 tangentToLocal = float4x4(
+			vTangent.x, vBinormal.x, vNormal.x,pos.x,
+			vTangent.y, vBinormal.y, vNormal.y,pos.y,
+			vTangent.z, vBinormal.z, vNormal.z,pos.z,
+			0,0,0,1
+		);
+
+		float4x4 facingRotationMatrix = AngleAxis4x4(rand(pos) * UNITY_TWO_PI, float3(0, 0, 1));
+		float4x4 bendRotationMatrix = AngleAxis4x4(rand(pos.zzx) * _BendRotationRandom * UNITY_PI * 0.5, float3(-1, 0, 0));
+		float4x4 transformationMatrix = mul(mul(tangentToLocal, facingRotationMatrix),bendRotationMatrix);
+
+		float height = (rand(pos.yyz) *2 - 1) * _BladeHeightRandom + _BladeHeight;
+		float width = (rand(pos.xzy) * 2 - 1) * _BladeWidthRandom + _BladeWidth;
+
+		triStream.Append(VertexOutput(mul(transformationMatrix, float4(width, 0, 0,1)),float2(0,0)));
+		triStream.Append(VertexOutput(mul(transformationMatrix, float4(-width, 0, 0,1)),float2(1,0)));
+		triStream.Append(VertexOutput(mul(transformationMatrix, float4(0, 0,height,1)),float2(0.5,1)));
+
+	}
+
+	ENDCG
+
+    SubShader
+    {
+		Cull Off
+
+        Pass
+        {
+			Tags
+			{
+				"RenderType" = "Opaque"
+				"LightMode" = "ForwardBase"
+			}
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#pragma geometry geo
+			#pragma target 4.6
+			#pragma hull hull
+			#pragma domain domain
+			#include "Lighting.cginc"
+
+			float4 _TopColor;
+			float4 _BottomColor;
+			float _TranslucentGain;
+
+			float4 frag (geometryOutput i, fixed facing : VFACE) : SV_Target
+            {	
+				return lerp(_BottomColor,_TopColor,i.uv.y);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Scripts/PositionReporterContro.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionReporterContro : MonoBehaviour
+{
+    [Header("Planes")]
+    [SerializeField]
+    GameObject grassPlane;
+    MeshRenderer grassRenderer;
+
+    [SerializeField]
+    GameObject lastPlane;
+    MeshRenderer DebugRenderer;
+    [Header("Shader")]
+    [SerializeField]
+    ComputeShader textureCompute;
+    [Header("Behaviours")]
+    [SerializeField]
+    [Range(0, 1)]
+    float circleDistance = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    float speed = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    float tailLength;
+    //Render Textures
+    public RenderTexture grassTexCurr;
+    public RenderTexture grassTexLast;
+    public RenderTexture slopeTexture;
+
+    //Properties IDs
+    int grassTexID = 0;
+    int debugTexID = 0;
+    
+    //Movement Reset Control
+    Vector3 initPosition = new Vector3(0.0f, 0.0f, 0.0f);
+
+    //Kernel IDs
+    int positionComputeID = 0;
+    int texCopyID = 0;
+    int slopeComputeID = 0;
+
+    //Movement state
+    Vector3 posAfterWritingTex;
+    float deltaMovement = 0.0f;
+    private void Awake()
+    {
+        //Set the grass renderer mat and set texture
+        grassRenderer = grassPlane.GetComponent<MeshRenderer>();
+        grassTexID = grassRenderer.material.shader.FindPropertyIndex("_MainTex");
+        
+        //Initialize all compute shader kernel IDs
+        positionComputeID = textureCompute.FindKernel("CSMain");
+        texCopyID = textureCompute.FindKernel("CopyTexture");
+        slopeComputeID = textureCompute.FindKernel("ComputeSlope");
+
+        //Create all render texture references used to store infomation of current frame,
+        //last frame and final slop map
+        grassTexCurr = new RenderTexture(1024, 1024, 0, RenderTextureFormat.R8);
+        grassTexCurr.enableRandomWrite = true;
+        grassTexCurr.Create();
+
+        grassTexLast = new RenderTexture(1024, 1024, 0, RenderTextureFormat.R8);
+        grassTexLast.enableRandomWrite = true;
+        grassTexLast.Create();
+
+        slopeTexture = new RenderTexture(1024, 1024, 0, RenderTextureFormat.RG16);
+        slopeTexture.enableRandomWrite = true;
+        slopeTexture.Create();
+
+        //Set the properties of the texture compute kernel
+        textureCompute.SetTexture(positionComputeID, "Result", grassTexCurr);
+        textureCompute.SetFloat("gradientDistance", circleDistance);
+        textureCompute.SetFloat("tailLength", tailLength);
+        textureCompute.SetTexture(positionComputeID, "LastTex", grassTexLast);
+
+        //Set the properties of texture copy kernel
+        textureCompute.SetTexture(texCopyID, "Result", grassTexCurr);
+        textureCompute.SetTexture(texCopyID, "LastTex", grassTexLast);
+
+        //Set the properties of Compute Slope kernel
+        textureCompute.SetTexture(slopeComputeID, "Result", grassTexCurr);
+        textureCompute.SetTexture(slopeComputeID, "OutputSlope", slopeTexture);
+
+        //Assign the debug plane
+        DebugRenderer = lastPlane.GetComponent<MeshRenderer>();
+        debugTexID = DebugRenderer.material.shader.FindPropertyIndex("_MainTex");
+    }
+    void Start()
+    {
+        initPosition = this.gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        SimpleMovement();
+        PositionTransfer();
+        SetPlaneTex();
+        deltaMovement = (this.gameObject.transform.position - posAfterWritingTex).magnitude;
+
+        textureCompute.Dispatch(positionComputeID, 1024 / 16, 1024 / 16, 1);
+        textureCompute.Dispatch(texCopyID, 1024 / 16, 1024 / 16, 1);
+        textureCompute.Dispatch(slopeComputeID, 1024 / 16, 1024 / 16, 1);
+
+        posAfterWritingTex = this.gameObject.transform.position;
+    }
+
+    void SimpleMovement()
+    {
+        this.gameObject.transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed,0.0f , Input.GetAxis("Vertical") * speed));
+        if (Input.GetKey(KeyCode.R))
+            this.gameObject.transform.position = initPosition;
+    }
+    void PositionTransfer()
+    {
+        Vector4 currPos = this.transform.position;
+        currPos.y = currPos.z;
+        Vector3 topRight = grassRenderer.bounds.max;
+        Vector3 buttomLeft = grassRenderer.bounds.min;
+        float width = topRight.x - buttomLeft.x;
+        float height = topRight.z - buttomLeft.z;
+        currPos = new Vector4((currPos.x + width / 2.0f) / width, (currPos.y + height / 2.0f) / height, 0, 0);
+        textureCompute.SetVector("currentPosition", currPos);
+    }
+    void SetPlaneTex()
+    {
+        grassRenderer.material.SetTexture(grassTexID, grassTexCurr);
+        DebugRenderer.material.SetTexture(debugTexID, slopeTexture);
+    }
+}
